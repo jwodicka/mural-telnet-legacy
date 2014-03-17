82aff8c7bc8f17d7a83b50fa8449728a854665cb
Kai Wodicka <kai@wodicka.net>
2014-03-16 20:17:54 -0700

    %world command, error handling.
    The %world command exists. It is naive and believes you when you tell it a
    world exists. It has no security at all. There is some error handling
    around using % commands from the unauthed shell and a stub for the
    unauthed shell helpfile.

--- telnet.server/server.js
+++ telnet.server/server.js
@@ -4,28 +4,49 @@ var byline = require('byline');
 
 var start = function(args, callback){
   var server = net.createServer(function(connection){
-    log.info('Connection established');
+    connection.randomNumber = Math.random();
+    log.info('Connection established: ' + connection.randomNumber);
+ //   connection.setTimeout(500,function(){ 
+ //     log.info('Connection timed out' + connection.randomNumber); 
+ //   });
     var stream = byline.createStream(connection);
     connection.user = null;
+    connection.activeRemote = null;
     // We have a connection object. It is a socket.
     // We want to bundle it plus a few other things to make an Endpoint.
     connection.on('end', function(){
-      log.info('Connection ended');
+      log.info('Connection ended' + connection.randomNumber);
     }); 
     stream.on('data', function(line){
       log.info('Data Received: '+ line.toString());
-      var words = line.toString().split(' ');
-       
+      var lineAsString = line.toString();
       if(connection.user) {
         // There is an authenticated user.
-	
+	if(lineAsString.charAt(0) === '%') {
+          // This is our special char! We care about this line, yay!
+	  // TODO: This needs to care about security
+          var firstWord = lineAsString.slice(0, lineAsString.indexOf(' '));
+	  if(firstWord === '%world') {
+            activeRemote = lineAsString.slice(lineAsString.indexOf(' ') + 1);
+	  } else {
+            connection.write('Huh?\n');
+	  }
+	} else {
+	  // This is meant for a remote.
+	  args['publish']('comm.' + activeRemote, lineAsString);
+	}
       } else {
+        var words = lineAsString.split(' ');
         if(words[0] == 'connect') {
           log.info('Authentication request');
           connection.user = (args['authenticate']({username: words[1], password: words[2]}));
 	  args['subscribe']('user.' + connection.user.toString());
           connection.write(connection.user.toString() + '\n');
-        }
+        } else if(words[0] == 'help') {
+	  connection.write('Have some helpfile. Only don\'t.\n');
+	} else {
+	  connection.write('Huh? \"connect <username> <password>\" or type \"help\"\n');
+	}
       }
     });
     connection.write('Welcome to Mural!\n');
--- test/telnet.server.js
+++ test/telnet.server.js
@@ -8,19 +8,28 @@ var net = require('net');
 var port = 8888;
 var authStub;
 var subStub;
+var pubStub;
 
 describe ('Telnet Server', function(){
   before(function(done){
     // server.start should take an associative array of values (including a port number) and a callback,  and execute the callback once the server is running. 
     authStub = sinon.stub().returns('testUser');
     subStub = sinon.stub().returns();
-    server.start({port: port, authenticate: authStub, subscribe: subStub}, function() {
+    pubStub = sinon.stub().returns(1);
+    server.start({port: port, authenticate: authStub, subscribe: subStub, publish: pubStub}, function() {
       done();
     })
   }); 
 
+  beforeEach(function(){
+    authStub.reset();
+    pubStub.reset();
+    subStub.reset();
+  });
+
   it('should accept a connection', function(done){
     var client = net.connect({port: port}, function(){
+      client.end();
       done();
     });
   });
@@ -29,7 +38,9 @@ describe ('Telnet Server', function(){
     // This does NOT test whether multiple simultaneous connections are possible; they may be created in series.
     var client1 = net.connect({port: port}, function(){
       var client2 = net.connect({port: port}, function(){
-        done();
+        client1.end();
+	client2.end();
+	done();
       });
     });
   });
@@ -38,12 +49,12 @@ describe ('Telnet Server', function(){
     var client = net.connect({port: port});
     client.on('data', function(data){
       data.toString().should.match(/Welcome to Mural/);
+      client.end();
       done();
     });
   });
 
   it('should pass credentials to the auth service', function(done){
-    authStub.reset();
     var client = net.connect({port:port}, function(connect){
       client.on('data', function(data){
 	if(data.toString().match(/testUser/)) { 
@@ -52,11 +63,25 @@ describe ('Telnet Server', function(){
 	}
       });
       client.write('connect testUser testPassword\n');
+      client.end();
+    });
+  });
+  
+  it('should publish messages to a remote PoP', function(done){
+    var client = net.connect({port:port}, function(connect){
+      client.on('close', function(hadError){
+        pubStub.calledWith('comm.TestWorld', 'Test Line').should.be.okay;	
+        hadError.should.be.false;
+	done(); 
+      });
+      client.write('connect testUser testPassword\n');
+      client.write('%world TestWorld\n');
+      client.write('Test Line\n');
+      client.end();
     });
   });
 
   it('should not pass credentials twice', function(done){
-    authStub.reset();
     var client = net.connect({port: port}, function(connect){
       client.on('data', function(data){
         if(data.toString().match(/testUser/)) {
@@ -65,6 +90,58 @@ describe ('Telnet Server', function(){
 	}
       });
       client.write('connect testUser testPassword\n');
+      client.end();
+    });
+  });
+
+  it('displays error to user at unknown system command', function(done){
+    var client = net.connect({port: port}, function(connect){
+      client.on('data', function(data){
+        if(data.toString().match(/Huh\?/)) {
+          done();
+	}
+      });
+      client.write('connect testUser testPassword\n');
+      client.write('%SHPROGLE!\n');
+      client.end();
+    });
+  });
+  
+  it('does not accept system commands without an authenticated user', function(done){
+    var client = net.connect({port: port}, function(connect){
+      client.on('data', function(data){
+        if(data.toString().match(/Huh\? \"connect/)){      
+  	  done();
+        }
+      });
+      client.write('%world TestWorld\n');
+      client.end();
+    });
+  });
+
+  it('displays help when prompted at the base shell', function(done){
+    var client = net.connect({port: port}, function(connect){
+      client.on('data', function(data){
+        if(data.toString().match(/helpfile/)){
+          done();
+        }
+      });
+      client.write('help\n');
+      client.end();
+    });
+  });
+
+  it('should subscribe to the user channel when authenticated', function(done){
+    var client = net.connect({port: port}, function(connect){
+      client.on('data', function(data){
+        if(data.toString().match(/testUser/)) {
+          authStub.calledOnce.should.be.okay;
+	  subStub.calledWith('user.testUser').should.be.okay;
+	  done();
+	}
+      });
+      client.write('connect testUser testPassword\n');
+      client.end();
     });
   });
 });
